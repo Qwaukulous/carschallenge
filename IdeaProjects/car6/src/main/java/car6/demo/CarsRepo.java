99b3c6fcac7c3e3aa92856184219be4d3d5a2a14
qwaukulous <qwauknation@yahoo.com>
2019-05-24 16:14:41 -0400

    .

---
+++ IdeaProjects/car6/src/main/java/car6/demo/CarsRepo.java
@@ -0,0 +1,6 @@
+package car6.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarsRepo extends CrudRepository<Cars,Long> {
+}
---
+++ IdeaProjects/car6/src/main/java/car6/demo/CatagoryRepo.java
@@ -0,0 +1,6 @@
+package car6.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatagoryRepo extends CrudRepository<Catagory,Long> {
+}
